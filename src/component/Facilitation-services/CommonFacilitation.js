b0f6040bfef6a1bc60f3f3672613133cf9ce6da2
ZenithForex-Navnit <it.developer3@zenithforex.com>
2025-11-25 11:20:56 +0530

    a

--- src/component/Facilitation-services/CommonFacilitation.js
+++ src/component/Facilitation-services/CommonFacilitation.js
@@ -4,6 +4,7 @@ import { Row, Col } from "react-bootstrap";
 function CommonFacilitation() {
   return (
     <>
+    
       <div className="ctp-money-transfer-area pb-75">
         <div className="shadow-inner">
           <div className="container">
